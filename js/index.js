1076c31092759b134851398fe48027b008050050
shashwat1998 <shashwat.pandey.1901@gmail.com>
2020-03-19 21:27:10 +0530

    Add initial files
    Added header and initial alert to welcome user
    Started the project
    Need to add background and home to make nav bar visible

---
+++ js/index.js
@@ -0,0 +1,21 @@
+swal({
+    title: "Hello World!!!",
+    text: "Hello visitor, you have landed upon my little webspace. I hope you're doing well."
+    // buttons: ["Nope, I'm just looking around.", "Yes, I'm hiring :)"]
+    // buttons: {
+    // 	cancel: {
+    // 	  text: "Nope.",
+    // 	  value: false,
+    // 	  visible: true,
+    // 	  className: "button-cancel",
+    // 	  closeModal: true,
+    // 	},
+    // 	confirm: {
+    // 	  text: "Yes, I'm hiring.",
+    // 	  value: true,
+    // 	  visible: true,
+    // 	  className: "button-confirm",
+    // 	  closeModal: true
+    // 	}
+    // }
+});
